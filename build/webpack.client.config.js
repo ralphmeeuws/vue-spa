20eee7f08854688afaeb59091dafad4abe215eab
Ralph Meeuws <ralph.meeuws@efocus.nl>
2018-04-05 17:53:43 +0200

    Minor corrections

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const objClientConfig = Object.assign({}, objBaseConfig, {
   plugins: (objBaseConfig.plugins || []).concat([
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
+      name: 'vendorScripts',
       filename: 'assets/js/[name].js'
     })
   ])
